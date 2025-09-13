3578bfbdc50b073d8c65d32127a79356884f7e9b
dawitphotos1 <dawitphotos1@gmail.com>
2025-09-13 06:13:56 -0400

    Add table creation and test course in server.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,4 @@
 
-// // backend/routes/auth.js
-// const express = require("express");
-// const authController = require("../controllers/authController");
-// const auth = require("../middleware/auth"); // middleware to protect routes
-
-// const router = express.Router();
-
-// // 🔹 Public routes
-// router.post("/register", authController.register);
-// router.post("/login", authController.login);
-
-// // 🔹 Protected routes
-// router.get("/me", auth, authController.me);
-
-// // 🔹 Admin-only route (approve/reject students)
-// router.put(
-//   "/students/:studentId/approval",
-//   auth, // requires logged-in admin
-//   authController.approveStudent
-// );
-
-// module.exports = router;
-
-
-
-
 // backend/routes/auth.js
 const express = require("express");
 const authController = require("../controllers/authController");
