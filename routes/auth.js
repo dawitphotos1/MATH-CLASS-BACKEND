42cde641a725318eec1dc9766e0987acb9b851a7
dawitphotos1 <dawitphotos1@gmail.com>
2025-09-17 17:33:32 -0400

    Add table creation and test course in server.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,36 +1,3 @@
-
-
-// // routes/auth.js
-// const express = require("express");
-// const authController = require("../controllers/authController");
-// const authMiddleware = require("../middleware/authMiddleware");
-// const validateRequest = require("../middleware/validateRequest");
-// const {
-//   registerValidation,
-//   loginValidation,
-// } = require("../validators/authValidator");
-
-// const router = express.Router();
-
-// // 🔹 Register
-// router.post(
-//   "/register",
-//   registerValidation,
-//   validateRequest,
-//   authController.register
-// );
-
-// // 🔹 Login
-// router.post("/login", loginValidation, validateRequest, authController.login);
-
-// // 🔹 Current User (me)
-// router.get("/me", authMiddleware, authController.me);
-
-// module.exports = router;
-
-
-
-
 // routes/auth.js
 const express = require("express");
 const authController = require("../controllers/authController");
