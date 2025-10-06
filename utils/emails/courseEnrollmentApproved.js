f499b255aa5a74cf884481130804da83084d5188
dawitphotos1 <dawitphotos1@gmail.com>
2025-10-05 23:35:57 -0400

    Add table creation and test course in server.js

--- utils/emails/courseEnrollmentApproved.js
+++ utils/emails/courseEnrollmentApproved.js
@@ -1,11 +1,3 @@
-// module.exports = (user, course) => {
-//   return {
-//     subject: `Enrollment Approved: ${course.title}`,
-//     text: `Hi ${user.name},\n\nYour enrollment in the course "${course.title}" has been approved!\n\nHappy learning!`,
-//     html: `<p>Hi ${user.name},</p><p>Your enrollment in <strong>${course.title}</strong> has been approved!</p><p>Happy learning!</p>`,
-//   };
-// };
-
 
 // utils/emails/courseEnrollmentApproved.js
 export default (user, course) => {
