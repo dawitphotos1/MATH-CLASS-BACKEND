735c88ffbdad36a3f481545b6e0c5ed53ee28210
dawitphotos1 <dawitphotos1@gmail.com>
2025-10-06 20:18:01 -0400

    Add table creation and test course in server.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,14 +1,3 @@
-// // routes/paymentRoutes.js
-// const express = require("express");
-// const router = express.Router();
-// const { createCheckoutSession } = require("../controllers/paymentController");
-
-// router.post("/create-checkout-session", createCheckoutSession);
-
-// module.exports = router;
-
-
-
 
 import express from "express";
 import { confirmPayment } from "../controllers/paymentConfirmController.js";
