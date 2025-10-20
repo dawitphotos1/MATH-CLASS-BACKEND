8a64e70a79d686227343d959d3e1638bd36c6f09
dawitphotos1 <dawitphotos1@gmail.com>
2025-10-20 07:23:09 -0400

    Add table creation and test course in server.js

--- migrations/20251020010029-add-category-to-courses.cjs
+++ migrations/20251020010029-add-category-to-courses.cjs
@@ -1,18 +1,3 @@
-// 'use strict';
-// /** @type {import('sequelize-cli').Migration} */
-// export async function up(queryInterface, Sequelize) {
-//   await queryInterface.addColumn('courses', 'category', {
-//     type: Sequelize.STRING,
-//     allowNull: true,
-//     defaultValue: 'General',  // optional — remove if not needed
-//   });
-// }
-
-// export async function down(queryInterface, Sequelize) {
-//   await queryInterface.removeColumn('courses', 'category');
-// }
-
-
 
 "use strict";
 
--- migrations/20251020095849-add-content-type-to-lessons.cjs
+++ migrations/20251020095849-add-content-type-to-lessons.cjs
@@ -1,21 +1,3 @@
-// "use strict";
-
-// module.exports = {
-//   async up(queryInterface, Sequelize) {
-//     await queryInterface.addColumn("lessons", "content_type", {
-//       type: Sequelize.STRING,
-//       allowNull: true,
-//       defaultValue: "text",
-//     });
-//   },
-
-//   async down(queryInterface, Sequelize) {
-//     await queryInterface.removeColumn("lessons", "content_type");
-//   },
-// };
-
-
-
 
 "use strict";
 
