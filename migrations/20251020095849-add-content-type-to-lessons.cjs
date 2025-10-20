d2fc37abc809870f8a51cd1817aa0cd9c319cd7b
dawitphotos1 <dawitphotos1@gmail.com>
2025-10-20 06:03:23 -0400

    Add table creation and test course in server.js

---
+++ migrations/20251020095849-add-content-type-to-lessons.cjs
@@ -0,0 +1,34 @@
+// "use strict";
+
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.addColumn("lessons", "content_type", {
+//       type: Sequelize.STRING,
+//       allowNull: true,
+//       defaultValue: "text",
+//     });
+//   },
+
+//   async down(queryInterface, Sequelize) {
+//     await queryInterface.removeColumn("lessons", "content_type");
+//   },
+// };
+
+
+
+
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("lessons", "content_type", {
+      type: Sequelize.STRING,
+      allowNull: true,
+      defaultValue: "text",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("lessons", "content_type");
+  },
+};
