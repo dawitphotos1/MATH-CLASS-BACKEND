1a50d033ac806b4a8f31c93b6703b06ab1bbbd88
dawitphotos1 <dawitphotos1@gmail.com>
2025-10-21 18:53:18 -0400

    Add table creation and test course in server.js

--- server.js
+++ server.js
@@ -268,11 +268,12 @@ app.use(cookieParser());
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "http://127.0.0.1:3000",
+  "http://localhost:3001",
   "https://math-class-platform.netlify.app",
-  "https://math-class-website-backend-1.onrender.com",
-  "https://checkout.stripe.com",
+  "https://leafy-semolina-fc0934.netlify.app",
+  "https://math-class-website-frontend.onrender.com",
 ];
+
 
 // ✅ Correct, explicit CORS middleware
 app.use(
