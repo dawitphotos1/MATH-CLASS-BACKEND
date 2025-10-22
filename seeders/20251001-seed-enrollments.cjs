b88d82f9a2007a46e047fe3f191bda23e41dba0f
dawitphotos1 <dawitphotos1@gmail.com>
2025-10-21 20:49:07 -0400

    Add table creation and test course in server.js

--- seeders/20251001-seed-enrollments.cjs
+++ seeders/20251001-seed-enrollments.cjs
@@ -1,37 +1,3 @@
-// "use strict";
-
-// module.exports = {
-//   up: async (queryInterface, Sequelize) => {
-//     await queryInterface.bulkInsert(
-//       "enrollments",
-//       [
-//         {
-//           user_id: 1,
-//           course_id: 1,
-//           created_at: new Date(),
-//           updated_at: new Date(),
-//         },
-//         {
-//           user_id: 2,
-//           course_id: 1,
-//           created_at: new Date(),
-//           updated_at: new Date(),
-//         },
-//         // Add more seed data here...
-//       ],
-//       {}
-//     );
-//   },
-
-//   down: async (queryInterface, Sequelize) => {
-//     // Make sure this deletes only the seeded rows, or all rows if that's okay
-//     await queryInterface.bulkDelete("enrollments", null, {});
-//   },
-// };
-
-
-
-
 "use strict";
 
 module.exports = {
