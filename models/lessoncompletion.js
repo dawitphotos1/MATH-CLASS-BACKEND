4e6f62d91472cf4633d6ed8b9721ca40fd16056f
dawitphotos1 <dawitphotos1@gmail.com>
2025-11-06 18:42:08 -0500

    Add table creation and test course in server.js

--- models/lessoncompletion.js
+++ models/lessoncompletion.js
@@ -36,21 +36,43 @@
 
 
 
-
-
+//modeld/lessoncompletion.js
+// models/LessonCompletion.js
 export default (sequelize, DataTypes) => {
   const LessonCompletion = sequelize.define(
     "LessonCompletion",
     {
-      id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-      user_id: { type: DataTypes.INTEGER, allowNull: false },
-      lesson_id: { type: DataTypes.INTEGER, allowNull: false },
-      completed_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      lesson_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "lessons",
+          key: "id",
+        },
+      },
+      completed_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
     },
     {
       tableName: "lesson_completions",
+      timestamps: false,
       underscored: true,
-      timestamps: true,
     }
   );
 
@@ -58,12 +80,10 @@ export default (sequelize, DataTypes) => {
     LessonCompletion.belongsTo(models.User, {
       foreignKey: "user_id",
       as: "user",
-      onDelete: "CASCADE",
     });
     LessonCompletion.belongsTo(models.Lesson, {
       foreignKey: "lesson_id",
       as: "lesson",
-      onDelete: "CASCADE",
     });
   };
 
