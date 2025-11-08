8920841d1a8a2a356d4786c3b16390842e6838e4
dawitphotos1 <dawitphotos1@gmail.com>
2025-11-08 08:48:03 -0500

    Add table creation and test course in server.js

--- models/Teacher.js
+++ models/Teacher.js
@@ -1,33 +1,4 @@
 
-// // models/Teacher.js
-// export default (sequelize, DataTypes) => {
-//   const Teacher = sequelize.define(
-//     "Teacher",
-//     {
-//       id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-//       user_id: { type: DataTypes.INTEGER, allowNull: false, unique: true },
-//     },
-//     {
-//       tableName: "teachers",
-//       underscored: true,
-//       timestamps: true,
-//     }
-//   );
-
-//   Teacher.associate = (models) => {
-//     Teacher.belongsTo(models.User, {
-//       foreignKey: "user_id",
-//       as: "user",
-//     });
-//   };
-
-//   return Teacher;
-// };
-
-
-
-
-
 // routes/teacher.js
 import express from "express";
 import { 
