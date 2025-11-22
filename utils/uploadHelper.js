918df406a05de5d6bf5f3310efc93da20603bcdf
dawitphotos1 <dawitphotos1@gmail.com>
2025-11-22 08:41:28 -0500

    Add table creation and test course in server.js

--- utils/uploadHelper.js
+++ utils/uploadHelper.js
@@ -1,18 +1,3 @@
-// // utils/uploadHelper.js
-// import fs from "fs";
-// import path from "path";
-
-// export const uploadToStorage = async (file) => {
-//   const uploadPath = path.join("uploads", Date.now() + "-" + file.originalname);
-
-//   fs.writeFileSync(uploadPath, file.buffer);
-
-//   return `/${uploadPath}`;
-// };
-
-
-
-
 // utils/uploadHelper.js
 import fs from "fs";
 import path from "path";
