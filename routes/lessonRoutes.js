c5a8eb4d473fb3710169329544190ed647a22c12
dawitphotos1 <dawitphotos1@gmail.com>
2025-12-13 13:13:24 -0500

    Add table creation and test course in server.js

--- routes/lessonRoutes.js
+++ routes/lessonRoutes.js
@@ -1,46 +1,4 @@
-// // routes/lessonRoutes.js
-// import express from "express";
-// import lessonController from "../controllers/lessonController.js";
-// import uploadMiddleware from "../middleware/uploadMiddleware.js";
-// import { requireAuth, requireRole } from "../middleware/authMiddleware.js"; // adjust to your auth middleware
-
-// const router = express.Router();
-
-// // Create lesson (multipart)
-// router.post(
-//   "/course/:courseId/lessons",
-//   uploadMiddleware.uploadLessonFiles, // parses multipart into memory
-//   lessonController.createLesson
-// );
-
-// // Update lesson (multipart)
-// router.put(
-//   "/:lessonId",
-//   uploadMiddleware.uploadLessonFiles,
-//   lessonController.updateLesson
-// );
-
-// // Get lesson
-// router.get("/:id", lessonController.getLessonById);
-// router.get("/preview/course/:courseId", lessonController.getPreviewLessonForCourse);
-// router.get("/public-preview/:id", lessonController.getPublicPreviewByLessonId);
-
-// // Debug
-// router.get("/:lessonId/debug-file", lessonController.debugLessonFile);
-// router.post("/:lessonId/fix-file", lessonController.fixLessonFileUrl);
-
-// // Get lessons by course/unit
-// router.get("/course/:courseId/all", lessonController.getLessonsByCourse);
-// router.get("/unit/:unitId/all", lessonController.getLessonsByUnit);
-
-// // Delete
-// router.delete("/:lessonId", lessonController.deleteLesson);
-
-// export default router;
-
-
-
-
+// routes/lessonRoutes.js
 
 import express from "express";
 import lessonController from "../controllers/lessonController.js";
