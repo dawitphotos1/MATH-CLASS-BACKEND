8b99a084a1851bc944281c5f3fb264e1881526cd
dawitphotos1 <dawitphotos1@gmail.com>
2025-12-29 14:07:52 -0500

    Add table creation and test course in server.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,42 +1,4 @@
 
-// // utils/sendEmail.js - COMPLETELY FIXED FOR YAHOO
-
-// import nodemailer from "nodemailer";
-
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 587,
-//   secure: false,
-//   auth: {
-//     user: process.env.MAIL_USER,
-//     pass: process.env.MAIL_PASS,
-//   },
-// });
-
-// await transporter.verify();
-
-// const sendEmail = async ({ to, subject, html, text }) => {
-//   try {
-//     const info = await transporter.sendMail({
-//       from: `"Math Class Platform" <${process.env.MAIL_USER}>`,
-//       to,
-//       subject,
-//       html,
-//       text,
-//     });
-
-//     return { success: true, messageId: info.messageId };
-//   } catch (error) {
-//     console.error("EMAIL ERROR:", error);
-//     return { success: false, error: error.message };
-//   }
-// };
-
-// export default sendEmail;
-
-
-
-
 // utils/sendEmail.js - LAZY LOADED (Won't crash at startup)
 import nodemailer from "nodemailer";
 
